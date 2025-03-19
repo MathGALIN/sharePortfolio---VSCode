0e4fda49b01d5fe8a494ea3bdf5ff0ea61e85937
mathieu <galiniermathieu06@gmail.com>
2025-03-19 12:21:16 +0100

    test action simple

--- src/test/java/tp/metier/ActionSimpleTest.java
+++ src/test/java/tp/metier/ActionSimpleTest.java
@@ -16,7 +16,6 @@
 package tp.metier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
