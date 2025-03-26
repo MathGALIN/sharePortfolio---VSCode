9cdd871f7f1aeb6c2f3d40515badb195b802688a
rachid <rachidboube4@gmail.com>
2025-03-26 10:28:44 +0100

    javadoc sur la classe action composée

--- src/main/java/tp/metier/ActionComposee.java
+++ src/main/java/tp/metier/ActionComposee.java
@@ -19,24 +19,46 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * La classe \`ActionComposee\` représente une action composée de plusieurs actions simples.
+ * Elle permet de calculer la valeur de l'action composée en fonction des valeurs des actions simples qui la composent.
+ * Chaque action simple est associée à un pourcentage représentant sa part dans l'action composée.
  *
- * @author perussel
+ * \@author perussel
  */
 public class ActionComposee extends Action {
 
-    // attribut lien
-    Map<ActionSimple, Float> mapPanier;
+  /**
+   * Map associant chaque \`ActionSimple\` à son pourcentage dans l'\`ActionComposee\`.
+   */
+  Map<ActionSimple, Float> mapPanier;
 
-    public ActionComposee(String libelle) {
-        super(libelle);
-        this.mapPanier = new HashMap<>();
-    }
+  /**
+   * Constructeur de la classe \`ActionComposee\`.
+   *
+   * \@param libelle Le libellé de l'action composée.
+   */
+  public ActionComposee(String libelle) {
+      super(libelle);
+      this.mapPanier = new HashMap<>();
+  }
 
-    // Fonction pour enregistrer une action dans l'action composé
+    /**
+     * Enregistre une action simple dans l'action composée avec un pourcentage donné.
+     *
+     * \@param as L'action simple à ajouter.
+     * \@param pourcentage Le pourcentage de l'action simple dans l'action composée.
+     */
     public void enrgComposition(ActionSimple as, float pourcentage) {
         this.mapPanier.put(as, pourcentage);
     }
 
+
+    /**
+     * Calcule la valeur de l'action composée pour un jour donné.
+     *
+     * \@param j Le jour pour lequel calculer la valeur.
+     * \@return La valeur de l'action composée pour le jour donné.
+     */
     @Override
     public float valeur(Jour j) {
         float valeur;
@@ -51,6 +73,13 @@ public class ActionComposee extends Action {
         return valeur;
     }
 
+
+    /**
+     * Vérifie si cette action composée est égale à un autre objet.
+     *
+     * \@param obj L'objet à comparer.
+     * \@return \`true\` si les objets sont égaux, \`false\` sinon.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -63,6 +92,12 @@ public class ActionComposee extends Action {
         return super.equals(other) && this.mapPanier.equals(other.mapPanier);
     }
 
+
+    /**
+     * Calcule le hash code de l'action composée.
+     *
+     * \@return Le hash code de l'action composée.
+     */
     @Override
     public int hashCode() {
         return super.hashCode() * 31 + mapPanier.hashCode();
